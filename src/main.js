24a4194d50f55032ea93f0ab0e5adbfc9f4c09c2
libinquan <libinquan@u51.com>
2018-09-30 21:48:20 +0800

    this sis master

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import router from './router'
 import App from './App'
 
+// this is master
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 const vm = new Vue({
