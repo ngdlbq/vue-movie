079acbdcd9e9427fc0bc2191d171ad5864fdcc76
libinquan <libinquan@u51.com>
2018-09-30 21:47:43 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import router from './router'
 import App from './App'
 
+// this is feat_detail_study
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 const vm = new Vue({
